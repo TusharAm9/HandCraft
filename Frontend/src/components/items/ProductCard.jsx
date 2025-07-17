210c1a188f0fc7d9ad74c4a91fc06a167b51c77a
TusharAm9 <tusharbarmatfg@gmail.com>
2025-07-17 15:42:32 +0530

    badge fix

--- Frontend/src/components/items/ProductCard.jsx
+++ Frontend/src/components/items/ProductCard.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Badge } from "@/components/ui/Badge";
+import { Badge } from "@/components/ui/badge";
 import { ShoppingCart, Heart, Star } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
